10736641a090c37e2a1fd5d1e4376652d20fa637
nickshevr <nickshevr@google.com>
2017-01-26 18:48:36 +0300

    Added joinGame route

--- routes/game/index.js
+++ routes/game/index.js
@@ -1,11 +1,10 @@
 const _ = require('lodash');
 const crypto = require('crypto');
-const config = require('config');
 const User = require('../../models/user').User;
 const Game = require('../../models/game').Game;
 const Piece = require('../../models/piece').Piece;
+const Token = require('../../models/token').Token;
 const errors = require('../../errors');
-const passport = require('../../auth');
 
 exports.createGame = function (req, res, next) {
     const response = {};
@@ -19,6 +18,35 @@ exports.createGame = function (req, res, next) {
     })
     .then(() => {
         res.JSON(response);
+    })
+    .catch(next);
+};
+// @ToDo удалять токен после аппрува
+exports.joinGame = function (req, res, next) {
+    const gameId = req.params.gameId;
+    const token = req.body.token;
+
+    Token.find({
+        value: token,
+        type: Token.statics.INVITE_TYPE,
+        gameId
+    })
+    .limit(1)
+    .lean()
+    .then(approved => {
+        if (!approved[0]) {
+            return next(new errors.NotAcceptable('Wrong Token'));
+        }
+
+        return Game.findById(gameId);
+    })
+    .then(gameToUpdate => {
+        gameToUpdate.userList.push(req.user._id);
+
+        return gameToUpdate.save();
+    })
+    .then(() => {
+        res.JSON(`Added to ${gameId}`)
     })
     .catch(next);
 };
